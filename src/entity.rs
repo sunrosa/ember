b2bc20e0f71ab3a660a6d59fceb9aa9a2ababb9e
Ibis Sunrosa <drsunrosa@gmail.com>
2024-01-08 12:08:04 +0000

    Fire test

--- src/entity.rs
+++ src/entity.rs
@@ -561,6 +561,48 @@ mod test {
     #[test]
     fn target_temperature_2() {
         let fire = Fire::init().add_item(LargeLog).unwrap();
-        assert_approx_eq!(fire.target_temperature(), 428.534615)
+        assert_approx_eq!(fire.target_temperature(), 428.534615);
+    }
+
+    #[test]
+    fn fire() {
+        let mut fire = Fire::init()
+            .add_multiple_items(ItemId::SmallStick, 5)
+            .unwrap();
+        fire = fire.set_tick_resolution(5.0);
+        for i in 0..135 {
+            if i == 1 {
+                fire = fire.add_multiple_items(ItemId::MediumStick, 5).unwrap();
+            }
+            if i == 5 {
+                fire = fire.add_multiple_items(ItemId::MediumStick, 10).unwrap();
+            }
+            if i == 5 {
+                fire = fire.add_item(ItemId::MediumStick).unwrap();
+            }
+            if i == 6 {
+                fire = fire.add_item(ItemId::MediumLog).unwrap();
+            }
+            if i == 7 {
+                fire = fire.add_item(ItemId::MediumLog).unwrap();
+            }
+            if i == 8 {
+                fire = fire.add_multiple_items(ItemId::MediumLog, 2).unwrap();
+            }
+            if i == 20 {
+                fire = fire.add_multiple_items(ItemId::MediumLog, 6).unwrap();
+            }
+            if i == 40 {
+                fire = fire.add_multiple_items(ItemId::LargeLog, 1).unwrap();
+            }
+
+            fire = fire.tick_multiple(15);
+        }
+
+        assert_eq!(
+            fire.energy_remaining(),
+            0.0,
+            "The fire should have burned through the entirety of the fuel, but it did not."
+        );
     }
 }
